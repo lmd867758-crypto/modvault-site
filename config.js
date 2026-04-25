6fee469066ebf8efa3906c8f9e04ffcef54c399f
lmd867758-crypto <lmd867758@gmail.com>
2026-04-26 00:12:05 +0600

    Update SHEET_ID in config.js

--- config.js
+++ config.js
@@ -1,5 +1,14 @@
 window.APP_CONFIG = {
-  SHEET_ID: "1J1Usvj-YwhaZt74Mhf3U4QoCbDok2QcsiLf_GzRFENY",
+  
+  SHEET_ID: "1uKab8_kyec3wOHcIPKEotfIqXwIbliWh8X_erS36GOM",
+  
   BRAND: "ModVault",
+  
   COPYRIGHT: "© 2026 ModVault"
+    
 };
+
+
+
+
+
